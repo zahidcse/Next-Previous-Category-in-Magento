95b1223104235824f17b6fba1c018253d186ef75
zahidcse <zahiislink@gmail.com>
2015-08-20 16:16:31 +0600

    Update getnextpre.php

--- getnextpre.php
+++ getnextpre.php
@@ -1,22 +1,32 @@
-<?php $currentCategory = Mage::registry('current_category') ;
- $returndata = $this->getNextPreviousCategory($currentCategory);
- function getNextPreviousCategory($currentCategory){
-	 $parentcategories = Mage::getModel('catalog/category')->load($currentCategory->parent_id)->getChildrenCategories();
-	  $positoinindex = array();
-	  $count=0;
-	  foreach ($parentcategories as $pcateid) {
+<?php
 
-	  	$_pcate = Mage::getModel('catalog/category')->load($pcateid->getId());
-	  	if($currentCategory->getId()==$pcateid->getId()){
-	  		$data['cpos'] = $count;
-              
-	  		$data['preindex'] = $cpos-1;
-	  		$data['nextindex'] = $cpos+1;
-	  	}
+$currentCategory = Mage::registry('current_category');
+$returndata = $this->getNextPreviousCategory($currentCategory);
 
-	    $positoinindex[$count] = $_pcate->getURL();
-	    $count++;
-	  }
+function getNextPreviousCategory($currentCategory) {
+    $parentcategories = Mage::getModel('catalog/category')->load($currentCategory->parent_id)->getChildrenCategories();
+    $positoinindex = array();
+    $count = 0;
+    $data['cpos'] = $count;
 
-	 return  $data;
+    $data['preindex'] = 0;
+    $data['nextindex'] = 0;
+    if (count(parentcategories) > 0) {
+        foreach ($parentcategories as $pcateid) {
+
+            $_pcate = Mage::getModel('catalog/category')->load($pcateid->getId());
+            if ($currentCategory->getId() == $pcateid->getId()) {
+                $data['cpos'] = $count;
+
+                $data['preindex'] = $cpos - 1;
+                $data['nextindex'] = $cpos + 1;
+            }
+
+            $positoinindex[$count] = $_pcate->getURL();
+            $count++;
+        }
+    }
+
+    return $data;
 }
+
