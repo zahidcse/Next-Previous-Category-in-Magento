79253c545be5c171837df53e155d04bf0bca5f0b
zahidcse <zahiislink@gmail.com>
2015-08-20 16:13:31 +0600

    Create getnextpre.php
    first change

---
+++ getnextpre.php
@@ -0,0 +1,22 @@
+<?php $currentCategory = Mage::registry('current_category') ;
+ $returndata = $this->getNextPreviousCategory($currentCategory);
+ function getNextPreviousCategory($currentCategory){
+	 $parentcategories = Mage::getModel('catalog/category')->load($currentCategory->parent_id)->getChildrenCategories();
+	  $positoinindex = array();
+	  $count=0;
+	  foreach ($parentcategories as $pcateid) {
+
+	  	$_pcate = Mage::getModel('catalog/category')->load($pcateid->getId());
+	  	if($currentCategory->getId()==$pcateid->getId()){
+	  		$data['cpos'] = $count;
+              
+	  		$data['preindex'] = $cpos-1;
+	  		$data['nextindex'] = $cpos+1;
+	  	}
+
+	    $positoinindex[$count] = $_pcate->getURL();
+	    $count++;
+	  }
+
+	 return  $data;
+}
